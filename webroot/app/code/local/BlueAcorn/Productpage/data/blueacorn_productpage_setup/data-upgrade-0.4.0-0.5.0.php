1536b1733b2e1e01c60e2e17c36a2732a1afbab6
Jamie Smith <jamie.smith@blueacorn.com>
2015-07-27 08:17:05 -0400

    added install scripts for finance button link enabled and text. Added system config for the href link

---
+++ webroot/app/code/local/BlueAcorn/Productpage/data/blueacorn_productpage_setup/data-upgrade-0.4.0-0.5.0.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package BlueAcorn_Productpage
+ * @version 0.2.0
+ * @copyright Copyright (c) 2015 Blue Acorn, Inc.
+ */
+
+$installer = $this;
+$setup = new Mage_Eav_Model_Entity_Setup('core_setup');
+$installer->startSetup();
+
+$setup->addAttribute(Mage_Catalog_Model_Product::ENTITY, 'finance_link_text', array(
+    'group' => 'Details', //will add Attribute to any group under Product Information (General, Prices, Meta Info...)
+    'type' => 'text',
+    'backend' => '',
+    'frontend' => '',
+    'label' => 'Finance Button Text',
+    'input' => 'text',
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'visible' => true,
+    'required' => false,
+    'user_defined' => true,
+    'searchable' => false,
+    'filterable' => true,
+    'comparable' => false,
+    'visible_on_front' => true,
+    'visible_in_advanced_search' => true,
+    'used_in_product_listing' => true,
+    'unique' => false,
+) );
+
+$installer->endSetup();
+
