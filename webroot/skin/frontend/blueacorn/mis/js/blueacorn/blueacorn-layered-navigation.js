e6dd1d03cec34241d4c078150f3420dcd86445d3
Brian Boyd <brian.boyd@blueacorn.com>
2015-08-04 15:38:05 -0400

    general theming and functionality for expanding/collapsing of filters

---
+++ webroot/skin/frontend/blueacorn/mis/js/blueacorn/blueacorn-layered-navigation.js
@@ -0,0 +1,58 @@
+/**
+ * @package     Blueacorn/layeredNavigation
+ * @version     1.0
+ * @author      Blue Acorn <code@blueacorn.com>
+ * @copyright   Copyright © 2015 Blue Acorn.
+ */
+
+if(typeof ba === 'undefined') {
+    var ba = {};
+}
+
+function layeredNavigation(options) {
+    this.init(options);
+}
+
+jQuery(document).ready(function ($) {
+
+    layeredNavigation.prototype = {
+        init: function (options) {
+            this.settings = {
+                'moduleName': 'layeredNavigation'
+            };
+
+            // Overrides the default settings
+            ba.overrideSettings(this.settings, options);
+
+            // Start the debugger
+            ba.setupDebugging(this.settings);
+
+            if ( $('body').hasClass('catalog-category-view') ) {
+                this.toggleFilter();
+            }
+        },
+
+        toggleFilter: function(){
+
+            $('#narrow-by-list dt').on('click', function() {
+
+                var self = $(this),
+                    filters = 'current',
+                    items = 'current',
+                    itemBlock = 'dd';
+
+                self.toggleClass(filters);
+                self.next(itemBlock).toggleClass(items);
+
+            });
+
+        }
+    };
+
+    /**
+     * The parameter object is optional.
+     * Must be an object.
+     */
+    ba.layeredNavigation = new layeredNavigation({});
+
+});
