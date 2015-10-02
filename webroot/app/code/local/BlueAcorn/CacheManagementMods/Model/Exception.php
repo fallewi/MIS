4591acd39f25b870962a693d78bd098e713cd8cc
KernelKorn <ryan.corn@blueacorn.com>
2015-10-02 13:11:31 -0400

    Installed Cache Managmement Contorls module

---
+++ webroot/app/code/local/BlueAcorn/CacheManagementMods/Model/Exception.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package     BlueAcorn\CacheManagementMods
+ * @version
+ * @author      Blue Acorn, Inc. <code@blueacorn.com>
+ * @copyright   Copyright © 2015 Blue Acorn, Inc.
+ */
+class BlueAcorn_CacheManagementMods_Model_Exception extends Mage_Adminhtml_Model_System_Config_Backend_Serialized_Array
+{
+    protected $_eventPrefix = 'BlueAcorn_CacheManagementMods'; // this is completely arbitrary
+}
