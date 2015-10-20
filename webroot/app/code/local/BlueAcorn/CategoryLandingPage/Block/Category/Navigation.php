ff20a525941d61f27dc01f1c29b60660f0a9621d
KernelKorn <ryan.corn@blueacorn.com>
2015-10-20 14:44:21 -0400

    Fix issue where subcategories were not showing based on nav tree depth

--- webroot/app/code/local/BlueAcorn/CategoryLandingPage/Block/Category/Navigation.php
+++ webroot/app/code/local/BlueAcorn/CategoryLandingPage/Block/Category/Navigation.php
@@ -8,48 +8,47 @@ class BlueAcorn_CategoryLandingPage_Block_Category_Navigation extends Mage_Catal
         $_baHelper = Mage::helper('blueacorn_categorylandingpage');
         $childCat = Mage::getModel('catalog/category')->getCategories($_current->getId(), 0, true, true);
         $navDepth = $_baHelper->getLayeredNavDepth();
+
+        // Check to see if the current category has child categories
         if (count($childCat) > 0) {
-
             foreach ($childCat as $_category) {
-                $recurse_count = 0;
+                $recursiveCount = 0;
+                $htmlClass = $this->checkIfChild($_category);
 
-                $check_child_class = $this->check_child_par($_category, $recurse_count, $navDepth);
-
-                echo "<dt class='" . $check_child_class . "'>";
+                echo "<dt class='" . $htmlClass . "'>";
                 echo "<a href='" . $_helper->getCategoryUrl($_category) . "'>" . $_category->getName();
                 echo "</a>";
-                echo $this->check_child($_category, $recurse_count, $navDepth);
+                echo $this->getSubCategories($_category, $recursiveCount, $navDepth);
                 echo "</dt>";
-
             }
         }
     }
-    public function check_child($_category, &$recurse_count, $navDepth = 10){
-        $recurse_count += 1;
-        if ($recurse_count < $navDepth) {
+    public function getSubCategories($_category, &$recursiveCount, $navDepth = 10, $categoryLvl = 0){
+        $recursiveCount += 1;
+
+        if ($recursiveCount <= $navDepth || $categoryLvl <= $navDepth + 1) {
             $_helper = Mage::helper('catalog/category');
             $_subsubcategories = $_category->getChildrenCategories();
 
             if (count($_subsubcategories) > 0) {
                 echo "<dd>";
+
                 foreach ($_subsubcategories as $_subcate) {
+                    $htmlClass = $this->checkIfChild($_subcate);
 
-                    $check_child_class = $this->check_child_par($_subcate, $recurse_count, $navDepth);
-
-                    if($check_child_class == "child"){
-                        echo "<li class='" . $check_child_class . "'>";
+                    if($htmlClass == "child"){
+                        echo "<li class='" . $htmlClass . "'>";
                         echo "<a href='" . $_helper->getCategoryUrl($_subcate) . "'>" . $_subcate->getName();
                         echo "</a>";
-                        echo $this->check_child($_subcate, $recurse_count, $navDepth);
+                        echo $this->getSubCategories($_subcate, $recursiveCount, $navDepth);
                         echo "</li>";
                     } else {
-                        echo "<ul class='" . $check_child_class . "-child'>";
+                        echo "<ul class='" . $htmlClass . "-child'>";
                         echo "<a href='" . $_helper->getCategoryUrl($_subcate) . "'>" . $_subcate->getName();
                         echo "</a>";
-                        echo $this->check_child($_subcate, $recurse_count, $navDepth);
+                        echo $this->getSubCategories($_subcate, $recursiveCount, $navDepth, $_subcate->getLevel());
                         echo "</ul>";
                     }
-
                 }
                 echo "</dd>";
             } else {
@@ -58,14 +57,13 @@ class BlueAcorn_CategoryLandingPage_Block_Category_Navigation extends Mage_Catal
         }
     }
 
-    public function check_child_par($_category, &$recurse_count, $navDepth = 10){
+    public function checkIfChild($_category){
         $_subsubcats = $_category->getChildrenCategories();
 
-        if (count($_subsubcats) > 0 && $recurse_count < $navDepth - 1){
+        if (count($_subsubcats) > 0){
             return "parent";
         }else{
             return "child";
         }
     }
-
 }
