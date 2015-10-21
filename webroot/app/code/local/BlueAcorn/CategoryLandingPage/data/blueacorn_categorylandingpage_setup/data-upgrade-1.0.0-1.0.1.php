b2a3f251a108294b1b8fb8ea038ed4ccb698a357
Brian Boyd <brian.boyd@blueacorn.com>
2015-10-21 13:03:08 -0400

    Added div with class to CMS Block that is required for proper positioning

--- webroot/app/code/local/BlueAcorn/CategoryLandingPage/data/blueacorn_categorylandingpage_setup/data-upgrade-1.0.0-1.0.1.php
+++ webroot/app/code/local/BlueAcorn/CategoryLandingPage/data/blueacorn_categorylandingpage_setup/data-upgrade-1.0.0-1.0.1.php
@@ -1,7 +1,7 @@
 <?php
 Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
 
-$content = '<a href="{{config path="web/unsecure/base_url"}}<!--replace with url key-->"> <!-- Insert Image here --> </a>';
+$content = '<div class="category-promo-block"><a href="{{config path="web/unsecure/base_url"}}<!--replace with url key-->"> <!-- Insert Image here --> </a></div>';
 
 $staticBlock = array(
     'title' => 'Promo Block Template',
