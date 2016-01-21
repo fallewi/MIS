0434a08ebf27bd95925e12cab5a0f1d62315d60c
Sam Tay <sam.chong.tay@gmail.com>
2016-01-21 14:38:32 -0500

    Change map_required to use invariable source model

---
+++ webroot/app/code/local/BlueAcorn/SpecialPricing/Model/Entity/Attribute/Source/Map.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package     BlueAcorn\SpecialPricing
+ * @version     1.0.0
+ * @author      Sam Tay @ Blue Acorn, Inc. <code@blueacorn.com>
+ * @copyright   Copyright © 2016 Blue Acorn, Inc.
+ */
+class BlueAcorn_SpecialPricing_Model_Entity_Attribute_Source_Map
+    extends Mage_Eav_Model_Entity_Attribute_Source_Abstract
+{
+    const DISABLED = 0;
+    const EMAIL = 1;
+    const CALL = 2;
+
+    /**
+     * Get all attribute options
+     * @return array
+     */
+    public function getAllOptions()
+    {
+        if (!$this->_options) {
+            $this->_options = array(
+                array('value' => self::DISABLED, 'label' => 'Disabled'),
+                array('value' => self::EMAIL, 'label' => 'Requires MAP Email'),
+                array('value' => self::CALL, 'label' => 'Requires MAP Call')
+            );
+        }
+        return $this->_options;
+    }
+}
