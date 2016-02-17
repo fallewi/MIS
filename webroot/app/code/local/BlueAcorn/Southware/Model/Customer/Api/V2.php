dd21d5bd545b765d9911a1e5501964621f21f6b3
KernelKorn <ryan.corn@blueacorn.com>
2016-02-17 14:38:00 -0500

    Setup Southware API code

---
+++ webroot/app/code/local/BlueAcorn/Southware/Model/Customer/Api/V2.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package BlueAcorn_Southware
+ * @version 0.1.0
+ * @author  BlueAcorn
+ * @copyright Copyright (c) 2015 Blue Acorn, Inc.
+ */
+
+class BlueAcorn_Southware_Model_Customer_Api_V2 extends BlueAcorn_Southware_Model_Customer_Api
+{
+}
---
+++ webroot/app/code/local/BlueAcorn/Southware/Model/Order/Api/V2.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package BlueAcorn_SouthwareApi
+ * @version 0.1.0
+ * @author BlueAcorn
+ * @copyright Copyright (c) 2015 Blue Acorn, Inc.
+ */
+
+class BlueAcorn_Southware_Model_Order_Api_V2 extends BlueAcorn_Southware_Model_Order_Api
+{
+}
