4f319a50dc6096a7ee092f121801a25c7c5e7c90
Chris Potter <chris.potter@blueacorn.com>
2016-02-23 10:47:20 -0500

    Add first scripts to devops-functional-scripts from original devops skel repo.

---
+++ exclude-media.js
@@ -0,0 +1,10 @@
+var git = require('git-promise');
+
+describe("exclude:media", function() {
+    it("ensure media not included in commit", function(done) {
+        git('ls-files ' + APP_ROOT + '/media').then(function(standardout){
+            expect(standardout).toEqual(false);
+            done();
+        });
+    });
+});
---
+++ exclude-var.js
@@ -0,0 +1,10 @@
+var git = require('git-promise');
+
+describe("exclude:var", function() {
+    it("ensure var not included in commit", function(done) {
+        git('ls-files ' + APP_ROOT + '/var').then(function(standardout){
+            expect(standardout).toEqual(false);
+            done();
+        });
+    });
+});
