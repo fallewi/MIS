fd3b48e22a4fbda7a549987d0e86c809f6742c3c
Richard Heywood <richard@blueacorn.com>
2016-02-24 17:44:42 -0500

    Merge pull request #156 from KernelKorn/features/legacy_orders_881640
    Features/legacy orders 881640

--- webroot/app/code/local/BlueAcorn/Legacy/Block/Order/History.php
+++ webroot/app/code/local/BlueAcorn/Legacy/Block/Order/History.php
@@ -3,12 +3,24 @@
 class BlueAcorn_Legacy_Block_Order_History extends Mage_Core_Block_Template
 {
 
+    /**
+     * BlueAcorn_Legacy_Block_Order_History constructor.
+     */
     public function __construct()
     {
         parent::__construct();
         $this->setTemplate('blueacorn/legacy/order/history.phtml');
 
-        $orders = Mage::getResourceModel('blueacorn_legacy/order_collection')
+        $orders = Mage::getResourceModel('sales/order_collection')
+            ->addFieldToSelect('*')
+            ->addFieldToFilter('customer_id', Mage::getSingleton('customer/session')->getCustomer()->getId())
+            ->addFieldToFilter('state', array('in' => Mage::getSingleton('sales/order_config')->getVisibleOnFrontStates()))
+            ->setOrder('created_at', 'desc')
+        ;
+
+        $this->setOrders($orders);
+
+        $legacyOrders = Mage::getResourceModel('blueacorn_legacy/order_collection')
             ->addFieldToSelect('*')
             ->addFieldToFilter('state', array('in' => Mage::getSingleton('sales/order_config')->getVisibleOnFrontStates()))
             ->setOrder('created_at', 'desc');
@@ -16,12 +28,12 @@ class BlueAcorn_Legacy_Block_Order_History extends Mage_Core_Block_Template
         $legacyId = Mage::getSingleton('customer/session')->getCustomer()->getLegacyCustomerId();
 
         if ($legacyId) {
-            $orders->addFieldToFilter('customer_id', $legacyId);
+            $legacyOrders->addFieldToFilter('customer_id', $legacyId);
         } else {
-            $orders->addFieldToFilter('entity_id', 0);
+            $legacyOrders->addFieldToFilter('entity_id', 0);
         }
 
-        $this->setOrders($orders);
+        $this->setLegacyOrders($legacyOrders);
 
         Mage::app()->getFrontController()->getAction()->getLayout()->getBlock('root')->setHeaderTitle(Mage::helper('sales')->__('My Orders'));
     }
@@ -37,26 +49,65 @@ class BlueAcorn_Legacy_Block_Order_History extends Mage_Core_Block_Template
         return $this;
     }
 
+    /**
+     * Gets Pager HTML
+     *
+     * @return string
+     */
     public function getPagerHtml()
     {
         return $this->getChildHtml('pager');
     }
 
+    /**
+     * Gets view link for order
+     *
+     * @param $order
+     * @return string
+     */
     public function getViewUrl($order)
+    {
+        return $this->getUrl('*/*/view', array('order_id' => $order->getId()));
+    }
+
+    /**
+     * Gets legacy order link
+     *
+     * @param $order
+     * @return string
+     */
+    public function getLegacyViewUrl($order)
     {
         return $this->getUrl('legacy/*/view', array('order_id' => $order->getId()));
     }
 
+    /**
+     * Gets order track url
+     *
+     * @param $order
+     * @return string
+     */
     public function getTrackUrl($order)
     {
         return $this->getUrl('*/*/track', array('order_id' => $order->getId()));
     }
 
+    /**
+     * Gets reorder url
+     *
+     * @param $order
+     * @return string
+     */
     public function getReorderUrl($order)
     {
         return $this->getUrl('*/*/reorder', array('order_id' => $order->getId()));
     }
 
+    /**
+     * Gets Back url
+     *
+     * @return string
+     */
     public function getBackUrl()
     {
         return $this->getUrl('customer/account/');
