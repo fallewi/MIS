70cc74dbc501fa10a67f91d98fd8b8c6ffd99aff
Richard Heywood <richard@blueacorn.com>
2016-03-15 17:33:16 -0400

    Update exclude-var.js

--- test.d/exclude-var.js
+++ test.d/exclude-var.js
@@ -3,7 +3,7 @@ var git = require('git-promise');
 describe("exclude:var", function() {
     it("ensure var not included in commit", function(done) {
         git('ls-files ' + APP_ROOT + '/var').then(function(standardout){
-            expect(standardout).toEqual(false);
+            expect(standardout).toBeFalsy();
             done();
         });
     });
