ed1ad07d9eff971fbb2f20f7d5cc7790d52eafaa
Justin Simon <justin.simon@blueacorn.com>
2016-03-18 12:38:25 -0400

    Initial commit Created module for Google Trusted Stores Created a new block class for the Trusted Stores order success block Created a separate template for the order success page block Added a layout update to insert the newly created block into the content block of checkout_onepage_success

---
+++ webroot/app/code/local/BlueAcorn/GoogleTrustedStores/Block/Onepage/Success.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @package     BlueAcorn\GoogleTrustedStores
+ * @version     1.0.0
+ * @author      Blue Acorn, Inc. <code@blueacorn.com>
+ * @copyright   Copyright © 2016 Blue Acorn, Inc.
+ */
+
+class BlueAcorn_GoogleTrustedStores_Block_Onepage_Success extends Mage_Checkout_Block_Onepage_Success {
+
+    protected function _construct()
+    {
+        parent::_construct();
+
+        if(!$this->hasData('order_id')) {
+            $this->setData('order_id', Mage::getSingleton('checkout/session')->getLastOrderId());
+        }
+    }
+
+    public function getOrderId() {
+        return $this->getData('order_id');
+    }
+
+    public function hasBackorderPreorder() {}
+
+    public function hasDigitalGoods() {
+        $hasDigitalGoods = 'N';
+        $orderId = $this->getOrderId();
+        $order = Mage::getModel('sales/order')->loadByIncrementId($orderId);
+
+        foreach($order->getAllItems() as $item) {
+            $product = $item->getProduct();
+            $productType = $product->getTypeId();
+
+            if($productType == Mage_Catalog_Model_Product_Type::TYPE_VIRTUAL) {
+                $hasDigitalGoods = 'Y';
+            }
+        }
+
+        return $hasDigitalGoods;
+    }
+
+    public function getCurrency() {
+        return Mage::app()->getStore()->getCurrentCurrencyCode();
+    }
+
+}
---
+++ webroot/app/code/local/BlueAcorn/GoogleTrustedStores/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package     BlueAcorn\GoogleTrustedStores
+ * @version     1.0.0
+ * @author      Blue Acorn, Inc. <code@blueacorn.com>
+ * @copyright   Copyright © 2016 Blue Acorn, Inc.
+ */
+class BlueAcorn_GoogleTrustedStores_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
