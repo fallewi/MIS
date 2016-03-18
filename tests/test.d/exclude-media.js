300b5d79eb0a9f40892110cfd7b03fe83ca039c9
Richard Heywood <richard@blueacorn.com>
2016-03-18 15:12:14 -0400

    removed old tests

--- tests/test.d/exclude-media.js
+++
@@ -1,9 +0,0 @@
-var git = require('git-promise');
-
-describe("exclude:media", function() {
-    it("ensure media not included in commit", function() {
-        git('ls-files ' + APP_ROOT + '/media').then(function(standardout){
-            expect(standardout).toEqual(false);
-        });
-    });
-});
--- tests/test.d/exclude-var.js
+++
@@ -1,9 +0,0 @@
-var git = require('git-promise');
-
-describe("exclude:var", function() {
-    it("ensure var not included in commit", function() {
-        git('ls-files ' + APP_ROOT + '/var').then(function(standardout){
-            expect(standardout).toEqual(false);
-        });
-    });
-});
