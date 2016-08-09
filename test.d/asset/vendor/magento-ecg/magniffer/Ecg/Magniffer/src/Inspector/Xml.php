f06287051c3a782dd61311caf5feb9a086b20e20
Richard Heywood <richard@blueacorn.com>
2016-08-09 16:39:24 -0400

    Squashed 'tests/' content from commit c2bf672
    git-subtree-dir: tests
    git-subtree-split: c2bf6722cc3a5172b81de206986ad550862c0340

---
+++ test.d/asset/vendor/magento-ecg/magniffer/Ecg/Magniffer/src/Inspector/Xml.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Ecg\Magniffer\Inspector;
+
+use Ecg\Magniffer\Inspector,
+    Ecg\Magniffer\Exception\InvalidXpathException,
+    Ecg\Magniffer\Report,
+    DOMDocument,
+    DOMXPath;
+
+class Xml extends Inspector
+{
+    /**
+     * @var DOMDocument
+     */
+    protected $dom;
+
+    /**
+     * @var DOMXPath
+     */
+    protected $domXpath;
+
+    /**
+     * @param array $patterns
+     * @param Report $report
+     */
+    public function __construct(array $patterns, Report $report)
+    {
+        parent::__construct($patterns, $report);
+        $this->dom = new DOMDocument();
+    }
+
+    /**
+     * @return bool
+     * @todo use Magento Finder component, like so: $file->isConfigXml()
+     */
+    public function canInspect()
+    {
+        return $this->file->getFilename() == 'config.xml';
+    }
+
+    /**
+     * @return Inspector
+     */
+    public function parse()
+    {
+        $this->dom->loadXML(file_get_contents($this->file->getRealPath()));
+        return $this;
+    }
+
+    /**
+     * @throws InvalidXpathException
+     * @return Inspector
+     */
+    public function inspect()
+    {
+        $this->domXpath = new DOMXPath($this->dom);
+        foreach ($this->patterns as $pattern) {
+            $xpath = $this->domXpath->query($pattern['xpath']);
+            if (!$xpath) {
+                throw new InvalidXpathException(sprintf('Invalid XPath "%s" given.', $pattern['xpath']));
+            }
+            foreach ($xpath as $node) {
+                $this->report->addIssue($this->file->getRealPath(), array(
+                    'line'      => $node->getLineNo(),
+                    'source'    => $node->C14N(),
+                    'message'   => $pattern['message'],
+                    'inspector' => get_class($this)
+                ));
+            }
+        }
+        return $this;
+    }
+}
