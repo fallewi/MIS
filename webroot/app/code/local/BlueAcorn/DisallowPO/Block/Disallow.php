2b800fc8e5f16311c9ad440ed7b2cda4816e6e37
KernelKorn <ryan.corn@blueacorn.com>
2016-08-24 10:23:36 -0400

    Install BA's Disallow PO Box module

---
+++ webroot/app/code/local/BlueAcorn/DisallowPO/Block/Disallow.php
@@ -0,0 +1,4 @@
+<?php
+class BlueAcorn_DisallowPO_Block_Disallow extends Mage_Core_Block_Template {
+
+}
---
+++ webroot/app/code/local/BlueAcorn/DisallowPO/Helper/Data.php
@@ -0,0 +1,4 @@
+<?php
+class BlueAcorn_DisallowPO_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
