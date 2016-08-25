b5fdf42de44c8adbf7e3ceffa209a333765550e4
KernelKorn <ryan.corn@blueacorn.com>
2016-08-25 10:53:16 -0400

    Add file docs

--- webroot/app/code/local/BlueAcorn/DisallowPO/Block/Disallow.php
+++ webroot/app/code/local/BlueAcorn/DisallowPO/Block/Disallow.php
@@ -1,4 +1,11 @@
 <?php
-class BlueAcorn_DisallowPO_Block_Disallow extends Mage_Core_Block_Template {
+/**
+ * @package BlueAcorn_DisallowPO
+ * @version 1.0.0
+ * @author BlueAcorn
+ * @copyright Copyright (c) 2016 Blue Acorn, Inc.
+ */
+class BlueAcorn_DisallowPO_Block_Disallow extends Mage_Core_Block_Template 
+{
 
 }
--- webroot/app/code/local/BlueAcorn/DisallowPO/Helper/Data.php
+++ webroot/app/code/local/BlueAcorn/DisallowPO/Helper/Data.php
@@ -1,4 +1,11 @@
 <?php
-class BlueAcorn_DisallowPO_Helper_Data extends Mage_Core_Helper_Abstract {
+/**
+ * @package BlueAcorn_DisallowPO
+ * @version 1.0.0
+ * @author BlueAcorn
+ * @copyright Copyright (c) 2016 Blue Acorn, Inc.
+ */
+class BlueAcorn_DisallowPO_Helper_Data extends Mage_Core_Helper_Abstract 
+{
 
 }
