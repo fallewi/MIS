bf92290343c620f7447d6e5bc2129c7e543c2039
Victor Cortez <victorc@missionrs.com>
2016-09-01 17:08:16 -0500

    1038533 Update Amasty Product Feed

--- webroot/app/code/local/Amasty/Feed/Model/Attribute/Compound/Isinstock.php
+++ webroot/app/code/local/Amasty/Feed/Model/Attribute/Compound/Isinstock.php
@@ -15,14 +15,16 @@
 
         protected function _getManageStock($productData)
         {
-            if (array_key_exists('use_config_manage_stock', $productData)) {
+            if (array_key_exists('use_config_manage_stock', $productData) &&
+                $productData['use_config_manage_stock'] === '1') {
                 if ($this->_configManageStock === null){
                     $this->_configManageStock = (int) Mage::getStoreConfigFlag(Mage_CatalogInventory_Model_Stock_Item::XML_PATH_MANAGE_STOCK);
                 }
                 return $this->_configManageStock;
             }
-            return array_key_exists('is_in_stock', $productData) &&
-            $productData['is_in_stock'] === '1' ? 1 : 0;
+
+            return array_key_exists('manage_stock', $productData) &&
+                $productData['manage_stock'] === '1' ? 1 : 0;
         }
 
         protected function _getIsInStock($productData)
