91f4d1f2ba70aa0047968fcfeb71e9b31dab7fb4
Victor Cortez <victorc@missionrs.com>
2017-01-13 16:45:36 -0600

    install Amasty dealer extension version 1.4.9

---
+++ webroot/app/code/local/Amasty/Perm/Model/Core/Email/Template/Pure.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Amasty Team
+ * @copyright Copyright (c) 2017 Amasty (https://www.amasty.com)
+ * @package Amasty_Perm
+ */
+if (Mage::helper('core')->isModuleEnabled('Amasty_Customerattr')) {
+    class Amasty_Perm_Model_Core_Email_Template_Pure extends Amasty_Customerattr_Model_Rewrite_Core_Email_Template {}
+} else {
+    class Amasty_Perm_Model_Core_Email_Template_Pure extends Mage_Core_Model_Email_Template {}
+}
